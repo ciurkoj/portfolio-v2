010fe93179cab35543cccdb35150ea9c8a5bbf08
ciurkoj <ciurkoj@outlook.com>
2020-09-18 00:44:50 +0200

    Updates

--- precache-manifest.3d61669e27237e453b8db648c14aa0e7.js
+++ precache-manifest.3d61669e27237e453b8db648c14aa0e7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "33e48247b0a921002e86078c9611e6b0",
+    "revision": "6617eb16e5843b6513b5c85b1135b177",
     "url": "/portfolio-v2/index.html"
   },
   {
@@ -12,12 +12,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio-v2/static/js/2.e8aee7d6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "72032415e830456d68da",
-    "url": "/portfolio-v2/static/js/main.19c7c0e4.chunk.js"
+    "revision": "01c4638abbdae5f1597a",
+    "url": "/portfolio-v2/static/js/main.099dfa92.chunk.js"
   },
   {
     "revision": "c251be4e9842592093e0098333b51410",
-    "url": "/portfolio-v2/static/js/main.19c7c0e4.chunk.js.LICENSE.txt"
+    "url": "/portfolio-v2/static/js/main.099dfa92.chunk.js.LICENSE.txt"
   },
   {
     "revision": "3992b0d658be16fa687d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-v2/precache-manifest.abf176928098ae70ab7b2fb0aebd6880.js"
+  "/portfolio-v2/precache-manifest.3d61669e27237e453b8db648c14aa0e7.js"
 );
 
 self.addEventListener('message', (event) => {
